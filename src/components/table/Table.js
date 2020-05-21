6d97ef30f57ca94d4ecc80c014e07ff83216cd83
Art <audiartik@gmail.com>
2020-05-21 18:41:49 +0300

    create table

---
+++ src/components/table/Table.js
@@ -0,0 +1,10 @@
+import {ExelComponent} from "@/ExelComponent";
+import {createTable} from "./table.template";
+
+export class Table extends ExelComponent {
+  static className = 'excel__table';
+
+  toHTML() {
+    return createTable(20);
+  }
+}
